48ac5dc67b2dcb2484a0d8392324ade3c337830f
dannertech <danner@danner.tech>
2023-04-27 22:27:09 -0400

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const Header = () => {
+    return(
+        <View>
+            <Text>This is the header</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Header;
